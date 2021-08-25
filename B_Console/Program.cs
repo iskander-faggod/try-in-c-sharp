e758d67d645998c0ca6d2ca6b5a20060da13cc54
Iskander <grinerplaydark@gmail.com>
2021-08-25 20:40:20 +0300

    for foreach

--- B_Console/Program.cs
+++ B_Console/Program.cs
@@ -4,7 +4,29 @@ namespace B_Console
 {
     internal class Program
     {
+
         public static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+
+            Console.WriteLine("-------------------");
+            
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        public static void DZ1(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            Console.WriteLine(firstNum > secondNum ? firstNum: secondNum);
+        }
+        public static void IfElse(string[] args)
         {
             int age = int.Parse(Console.ReadLine());
             double weight = double.Parse(Console.ReadLine());
