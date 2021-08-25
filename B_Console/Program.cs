417fc2b3925cc68c09d49413a543f7d766ade204
Iskander <grinerplaydark@gmail.com>
2021-08-25 19:54:37 +0300

    if + ternop

---
+++ B_Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace B_Console
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double weight = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double bodyMassIndex = weight / (height * height);
+
+            bool isToLow = bodyMassIndex <= 18.5;
+            bool isNormal = bodyMassIndex > 18.5 && bodyMassIndex < 25;
+            bool isAboveNormal = bodyMassIndex >= 25 && bodyMassIndex <= 30;
+            bool isTooFat = bodyMassIndex > 30;
+
+            bool isFat = isAboveNormal || isTooFat;
+
+            if (isFat)
+            {
+                Console.WriteLine("Вы толстенький");
+            }
+            else
+            {
+                Console.WriteLine("Все супер)");
+            }
+
+            string advice = age >= 18 ? "Можешь пить алкоголь" : "Не стоит пить братишка";
+
+        }
+    }
+}
--- B_Console/Properties/AssemblyInfo.cs
+++ B_Console/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("B_Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("B_Console")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("C0E11CE3-DCA9-4C93-AD3A-32A9254B1714")]
+[assembly: Guid("9FA32924-50B4-4D6A-B81E-09774CD94194")]
 
 // Version information for an assembly consists of the following four values:
 //
